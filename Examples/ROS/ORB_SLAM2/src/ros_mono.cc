966a336d91209c7262ada80d16479859c308199b
Jose-Luis Sanchez-Lopez <joseluis.sanlop@gmail.com>
2019-10-31 18:29:04 +0100

    adding tf publication

--- Examples/ROS/ORB_SLAM2/src/ros_mono.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_mono.cc
@@ -18,20 +18,37 @@
 * along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
 */
 
+//
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
 
-#include<iostream>
-#include<algorithm>
-#include<fstream>
-#include<chrono>
+// OPEN CV
+#include <opencv2/core/core.hpp>
 
-#include<ros/ros.h>
+// ORB_SLAM2
+#include "../../../include/System.h"
+
+// ROS
+#include <ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
 
-#include<opencv2/core/core.hpp>
+// Tf2
+#include <tf2/LinearMath/Transform.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_broadcaster.h>
 
-#include"../../../include/System.h"
+
 
 using namespace std;
+
+
+std::string parent_name;
+std::string child_name;
+
+tf2_ros::TransformBroadcaster* tf_transform_broadcaster;
+
 
 class ImageGrabber
 {
@@ -47,7 +64,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "Mono");
     ros::start();
-
+    
     if(argc != 3)
     {
         cerr << endl << "Usage: rosrun ORB_SLAM2 Mono path_to_vocabulary path_to_settings" << endl;        
@@ -60,9 +77,24 @@ int main(int argc, char **argv)
 
     ImageGrabber igb(&SLAM);
 
+  
+    // Parameters
+    //
+    ros::param::param<std::string>("~camera_base_link", parent_name,"camera_orb_slam2");
+    std::cout<<"camera_base_link="<<parent_name<<std::endl;
+    //
+    ros::param::param<std::string>("~world_base_link", child_name,"world_orb_slam2");
+    std::cout<<"world_base_link="<<child_name<<std::endl;
+
+
     ros::NodeHandle nodeHandler;
     ros::Subscriber sub = nodeHandler.subscribe("/camera/image_raw", 1, &ImageGrabber::GrabImage,&igb);
 
+
+    //
+    tf_transform_broadcaster=new tf2_ros::TransformBroadcaster();
+
+    //
     ros::spin();
 
     // Stop all threads
@@ -70,7 +102,12 @@ int main(int argc, char **argv)
 
     // Save camera trajectory
     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
-
+    
+    //
+    if(tf_transform_broadcaster)
+        delete tf_transform_broadcaster;
+    
+    //
     ros::shutdown();
 
     return 0;
@@ -90,7 +127,49 @@ void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
         return;
     }
 
-    mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
+    //
+    cv::Mat camera_pose_mat=mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
+    
+    
+    // Check
+    if(camera_pose_mat.empty())
+    {
+      ROS_DEBUG("Localization failed");
+      return;
+    }
+    
+    // Current Time Stamp
+    ros::Time curr_stamp = msg->header.stamp;
+    
+    
+    // Tf transform
+    tf2::Matrix3x3 tf_rot(camera_pose_mat.at<float>(0,0), camera_pose_mat.at<float>(0,1), camera_pose_mat.at<float>(0,2),
+                       camera_pose_mat.at<float>(1,0), camera_pose_mat.at<float>(1,1), camera_pose_mat.at<float>(1,2),
+                       camera_pose_mat.at<float>(2,0), camera_pose_mat.at<float>(2,1), camera_pose_mat.at<float>(2,2));
+
+    tf2::Vector3 tf_orig(camera_pose_mat.at<float>(0,3), camera_pose_mat.at<float>(1,3), camera_pose_mat.at<float>(2,3));
+
+    tf2::Transform tf2_transform=tf2::Transform(tf_rot, tf_orig);
+
+    geometry_msgs::Transform transform=tf2::toMsg(tf2_transform);
+    
+    
+    // TF
+    geometry_msgs::TransformStamped transform_stamped;
+
+    // Header
+    transform_stamped.header.stamp=curr_stamp;
+    transform_stamped.header.frame_id=parent_name;
+    // Child frame id
+    transform_stamped.child_frame_id=child_name;
+    // Transform
+    transform_stamped.transform = transform;
+    // Publish
+    tf_transform_broadcaster->sendTransform(transform_stamped);
+    
+    
+    //
+    return;
 }
 
 
